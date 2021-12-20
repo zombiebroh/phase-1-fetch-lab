1143f801b367dd781631815e1d17be7d9d4af9de
Gehrig Barnes <gehrigbarnes@gmail.com>
2021-12-20 11:55:40 -0600

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 function fetchBooks() {
-  fetch("https://anapioficeandfire.com/api/books")
-  .then((resp) => resp.json())
-  .then(json => renderBooks(json))
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const main = document.querySelector('main')
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+    const h2 = document.createElement('h2')
+    h2.innerHTML = book.name
+    main.appendChild(h2)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+  fetchBooks()
+})
