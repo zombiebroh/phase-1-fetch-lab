9d503f4d109579ffec1198c1453ad9380ef6e33e
Gehrig Barnes <gehrigbarnes@gmail.com>
2021-12-20 11:51:47 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then(json => renderBooks(json))
   
 }
 
