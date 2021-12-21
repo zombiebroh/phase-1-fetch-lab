f7dd8a040f792370aeb5bd04c26cc97046e0a7fd
Gehrig Barnes <gehrigbarnes@gmail.com>
2021-12-21 07:34:39 -0600

    commit tree d3f3e462444a3ada7957f9f0c5f6804c173447c7

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fetchBooks() {
   return fetch("https://anapioficeandfire.com/api/books")
     .then(resp => resp.json())
-    .then(json => renderBooks(json))
+    .then(data => renderBooks(data))
 }
 
 function renderBooks(books) {
